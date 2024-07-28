3474b35142985aa1799d3e708f0c200bd38dc592
asarama <arshameslami@hotmail.com>
2024-07-28 19:08:11 -0400

    Updated the select and text inputs to use tap instead

--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ async function main() {
     await navigateToUrl(mainPage, config.loginUrl)
     await performLogin(mainPage, config);
     await navigateToUrl(mainPage, config.roomUrl);
+
+    // Set Duration
+    await mainPage.tap('#facility-page-content > div.facility-selected-items.second-row > span > span > span.k-select > span')
+    await mainPage.waitForTimeout(500);
+    await mainPage.tap('#service-duration-dropdown_listbox > li:nth-child(4)')
+
+    // Set Number of people to attend
+    await mainPage.tap('#facility-page-content > div.number-of-people-input > span > span > span.k-select > span.k-link.k-link-increase')
+    await mainPage.waitForTimeout(1000);
+    await mainPage.tap('#facility-page-content > div.number-of-people-input > span > span > span.k-select > span.k-link.k-link-increase')
+    // No idea why I need a third tap
+    await mainPage.tap('#facility-page-content > div.number-of-people-input > span > span > span.k-select > span.k-link.k-link-increase')
+
+
     console.log('Login and navigation successful!');
   } catch (error) {
     console.error('An error occurred:', error);
